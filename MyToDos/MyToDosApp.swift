e101977186962685b3b0e862411ec04afd859ec3
Abdul Wahab <abdul.wahab@danatonline.com>
2021-08-18 18:59:48 +0500

    Initial Commit

---
+++ MyToDos/MyToDosApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyToDosApp.swift
+//  MyToDos
+//
+//  Created by Abdul Wahab on 8/18/21.
+//
+
+import SwiftUI
+
+@main
+struct MyToDosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
